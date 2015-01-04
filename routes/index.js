95d96b0970b04c5518610fad8926158620e3112c
M0unir <mounir.chouladi@gmail.com>
2015-01-04 01:38:06 -0800

    Express 4 app generation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Analytics Dashboard' });
 });
 
 module.exports = router;
